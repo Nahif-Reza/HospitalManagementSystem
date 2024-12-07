36ccc985d5f6e8c1278ab9a4707dcc2464d7fc78
Nahif Reza <118654543+Nahif-Reza@users.noreply.github.com>
2024-12-08 01:38:33 +0600

    Add files via upload

---
+++ Hospital Management System/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <nav class="navbar">
+        <div class="left-nav">
+            <h3 class="logo">This Is Logo</h3>
+        </div>
+
+        <div class="right-nav">
+            <input type="text" placeholder="Search" class="search">
+            <a href="#" class="login">Login</a>
+            <a href="#" class="registration">Registration</a>
+        </div>
+    </nav>
+
+    <div class="front">
+        <h1>Welcome to ___ Hospital</h1>
+        <p>Some information about the hospital</p>
+        <a href="take_user_info.php">Book an Appointment</a>
+    </div>
+</body>
+</html>
